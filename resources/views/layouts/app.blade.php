ecab2a5f587534eaf151b453a16ae1bf32b7f103
hugoresende27 <Hugoresende27@gmail.com>
2022-02-25 09:41:16 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet">
-<link href="/dist/output.css" rel="stylesheet">
+{{-- <link href="/dist/output.css" rel="stylesheet"> --}}
 <style>
     body { overflow-x: hidden; /* Hide scrollbars */ 
                
