cf7cfabffb6d6408c2e5e8e65b493e65bfdf8c69
hugoresende27 <Hugoresende27@gmail.com>
2022-03-02 19:10:26 +0000

    't'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use JD\Cloudder\Facades\Cloudder;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -21,17 +22,44 @@ class UserController extends Controller
 
         $auth_id = Auth::user()->id;
 
-        if (isset($request->image)){
+        if (isset($request->image))
+        {
             $imageName = time().'.'.$request->image->extension();  
             $destination = public_path('/profile_pics');
             $request->image->move($destination,$imageName);
-        } else {
+
+    //////////////////////////////////////////////////////////////////////////
+                // if ($request->isMethod('post')) {
+                  
+
+                    
+                $image_name = $request->file('image')->getPath();
+                // dd(get_defined_vars()); 
+        //the upload method handles the uploading of the file and can accept attributes to define what should happen to the image
+
+        //Also note you could set a default height for all the images and Cloudinary does a good job of handling and rendering the image.
+                Cloudder::upload($image_name, null, array(
+                    "folder" => "omega",  "overwrite" => FALSE,
+                    "resource_type" => "image", "responsive" => TRUE, "transformation" => array("quality" => "70", "width" => "250", "height" => "250", "crop" => "scale")
+                ));
+                    $public_id = Cloudder::getPublicId();
+
+                    $width = 250;
+                    $height = 250;
+
+                    $image_url = Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height" => $height, "crop" => "scale", "quality" => 70, "secure" => "true"]);
+            // }
+  //////////////////////////////////////////////////////////////////////////
+
+        } 
+        else 
+        {
             $imageName = User::where('id',$auth_id)->first();
             $imageName = $imageName->image;
         }
        
+       
         
-
         $user = User::where('id',$auth_id)->update([
                 'first_name' => $request->first_name,
                 'last_name' => $request->last_name,
@@ -41,7 +69,8 @@ class UserController extends Controller
                 'mobile' => $request->phone,
                 'image'=>$imageName
             ]);
-            
+
+           
       return redirect ('myprofile')->with('message','Profile Uploaded');
     }
 
