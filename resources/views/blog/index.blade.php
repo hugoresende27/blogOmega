0d022544d9a5fe2ddd7bb344ef5de48e7f3c11ec
hugoresende27 <Hugoresende27@gmail.com>
2022-03-02 15:30:22 +0000

    't'

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -45,32 +45,32 @@
         {{-- {{ dd($p->image_path) }} --}}
         <div class="all-posts">
             <div>
-                <img class="mt-10 rounded-2xl" src="{{ asset('/post_pics/'.$p->image) }}" width="700" alt="img_do_post">
+                <img class="mt-10 rounded-2xl" src="{{ url('/post_pics/'.$p->image) }}" width="700" alt="img_do_post">
             </div>
             <div>
                 <h2 class="text-green-300 font-bold text-5xl pb-4">
                     {{ $p->title }}
                 </h2>
-                <span class="text-green-600">
-                    De <span class="font-bold italic text-gray-100"> {{ $p->user->name }} </span>, criado em {{ date('h:i:sa Y-m-d', strtotime($p->updated_at)) }}
+                <span class="">
+                    De <span class=""> {{ $p->user->first_name }} </span>, criado em {{ date('h:i:sa Y-m-d', strtotime($p->updated_at)) }}
                 </span>
               
-                <p class=" text-xl text-green-400 pt-8 pb-10 leading-8 font-light">
+                <p class=" ">
                 
                     {{ $p->description }}
                 </p>
                 
-                <a href="/blog/{{ $p->slug }}" class="hover:text-black uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                <a href="/blog/{{ $p->slug }}" class="btn-hero-bt2">
                     Read more
                 </a>
 
                  {{------------- BOTÃO EDIT APENAS VISIVEL SE AUTENTICADO E AUTENTICADO CORRESPONDER AO AUTOR DO POST --}}
                 @if (isset(Auth::user()->id) && Auth::user()->id == $p->user_id)
 
-                <div class="relative mt-10">
+                <div class="but-ger">
                     <span class="">
-                        <a href="/blog/{{ $p->slug }}/edit" class="py-4 px-4 bg-green-500 rounded-3xl text-lg hover:text-white">
-                            Editar
+                        <a href="/blog/{{ $p->slug }}/edit" class="btn-hero-bt2">
+                            Edit
                         </a>
                     </span>
                 {{------------- BOTÃO DELETE APENAS VISIVEL SE AUTENTICADO E AUTENTICADO CORRESPONDER AO AUTOR DO POST --}}
@@ -78,7 +78,7 @@
                         <form action="/blog/{{ $p->slug }}" method="POST" class="mt-5">
                             @csrf
                             @method('delete')
-                            <button type="submit" class="mb-2 py-4 px-4 bg-red-500 rounded-3xl text-lg hover:text-white">
+                            <button type="submit" class="btn-hero-bt">
                                 Delete
                             </button>
                         </form>              
